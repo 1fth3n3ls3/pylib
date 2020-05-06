889298a2fe6faacfed838c89f1421880572523ff
David Alvarez <davidtcc@gmail.com>
2020-05-06 19:51:16 +0200

    commenting old code

--- hierarchyconvertermaya.py
+++ hierarchyconvertermaya.py
@@ -7,26 +7,32 @@ from PySide2 import QtCore
 
 _window = None
 
-def show():
-    global _window
-    if _window is None:
-        cont = gui.ConvertHierarchyController()
-        def emiSelChanged(_):
-            """This methods emits a list of selected nodes"""
-            cont.selectionChanged.emit(pmc.selected(type='transform'))
+# def show():
+#     global _window
+#     if _window is None:
+#         cont = gui.ConvertHierarchyController()
+#         def emiSelChanged(_):
+#             """This methods emits a list of selected nodes"""
+#             cont.selectionChanged.emit(pmc.selected(type='transform'))
         
-        om.MEventMessage.addEventCallback('SelectionChanged', emiSelChanged)
-        parent = utils.getMayaWindow()
-        _window = gui.Window(cont, parent=parent)
-        def onConvert(prefix):
-            settings = dict(charcreator.SETTINGS_DEFAULT, 
-                            prefix=unicode(prefix))
-            charcreator.convert_hierarchies_main(settings)
-        _window.convertPressed.connect(onConvert)
-    _window.show()
+#         om.MEventMessage.addEventCallback('SelectionChanged', emiSelChanged)
+#         parent = utils.getMayaWindow()
+#         _window = gui.Window(cont, parent=parent)
+#         def onConvert(prefix):
+#             settings = dict(charcreator.SETTINGS_DEFAULT, 
+#                             prefix=unicode(prefix))
+#             charcreator.convert_hierarchies_main(settings)
+#         _window.convertPressed.connect(onConvert)
+#     _window.show()
 
 
 class MayaController(QtCore.QObject):
+    """
+    # Call this way from shelve
+
+    from pylib.hierarchyconvertermaya import MayaController
+    MayaController()
+    """
     view = None
     selectionChanged = QtCore.Signal(list) # must be defined as class atributte
 
@@ -50,4 +56,7 @@ class MayaController(QtCore.QObject):
         settings = dict(charcreator.SETTINGS_DEFAULT, 
                         prefix=unicode(prefix))
         charcreator.convert_hierarchies_main(settings)
+
+
+	
 
